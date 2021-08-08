c00e44c9cb906af1dc1821f7b9c10de6243ee789
algins <igors.algins@gmail.com>
2021-08-08 11:25:34 +0300

    fix linting errors

--- app/helpers.php
+++ app/helpers.php
@@ -3,6 +3,9 @@
 if (! function_exists('normalize_url')) {
     function normalize_url(string $url): string
     {
-        return strtolower(parse_url($url, PHP_URL_HOST));
+        /** @var string $parsedUrl */
+        $parsedUrl = parse_url($url, PHP_URL_HOST);
+
+        return strtolower($parsedUrl);
     }
 }
