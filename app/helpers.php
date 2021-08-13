1135c77cc9d9569c7fed31e4d3182d0d46a42819
algins <igors.algins@gmail.com>
2021-08-13 07:25:47 +0300

    fix normnalize_url

--- app/helpers.php
+++ app/helpers.php
@@ -3,9 +3,6 @@
 if (! function_exists('normalize_url')) {
     function normalize_url(string $url): string
     {
-        /** @var string $parsedUrl */
-        $parsedUrl = parse_url($url, PHP_URL_HOST);
-
-        return strtolower($parsedUrl);
+        return strtolower($url);
     }
 }
